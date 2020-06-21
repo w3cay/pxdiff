9221eaa9be06dda4ccc3dce450e38d5e3b4dbdeb
w3cay <ay.chen@Ctrip.com>
2020-06-21 18:41:30 +0800

    feat: 优化交互

--- src/main/index.js
+++ src/main/index.js
@@ -1,55 +1,67 @@
-import { app, BrowserWindow } from 'electron'
-
+import { app, BrowserWindow, ipcMain } from "electron";
+const electronLocalshortcut = require("electron-localshortcut");
 /**
  * Set `__static` path to static files in production
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
  */
-if (process.env.NODE_ENV !== 'development') {
-  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+if (process.env.NODE_ENV !== "development") {
+  global.__static = require("path")
+    .join(__dirname, "/static")
+    .replace(/\\/g, "\\\\");
 }
 
-let mainWindow
-const winURL = process.env.NODE_ENV === 'development'
-  ? `http://localhost:9080`
-  : `file://${__dirname}/index.html`
+let mainWindow;
+const winURL =
+  process.env.NODE_ENV === "development"
+    ? `http://localhost:9080`
+    : `file://${__dirname}/index.html`;
 
-function createWindow () {
+function createWindow() {
   /**
    * Initial window options
    */
   mainWindow = new BrowserWindow({
     useContentSize: true,
     transparent: true,
+    minWidth: 100,
     width: 375,
-    minWidth: 375,
-    backgroundColor: '#00FFFFFF',
+    height: 600,
+    backgroundColor: "#00FFFFFF",
     webPreferences: {
-      webSecurity: false
-    }
-  })
+      webSecurity: false,
+    },
+  });
 
-  mainWindow.loadURL(winURL)
-  mainWindow.setBackgroundColor('#00FFFFFF')
-  mainWindow.on('closed', () => {
-    mainWindow = null
-  })
+  mainWindow.loadURL(winURL);
+  mainWindow.setBackgroundColor("#00FFFFFF");
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+
+  electronLocalshortcut.register(mainWindow, "Down", () => {
+    mainWindow.webContents.send('keyda', 'Down')
+  });
+
+  electronLocalshortcut.register(mainWindow, "Up", () => {
+    mainWindow.webContents.send('keyda', 'Up')
+  });
 }
 
-app.on('ready', createWindow)
+app.on("ready", createWindow);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
-app.commandLine.appendSwitch('enable-transparent-visuals');
+app.commandLine.appendSwitch("enable-transparent-visuals");
 
 /**
  * Auto Updater
--- src/renderer/components/MainWrap.vue
+++ src/renderer/components/MainWrap.vue
@@ -3,36 +3,31 @@
     <div class="cover" id="drag" @click="isSlideOpen = true">
       <img class="coverPic" :style="{opacity: form.opacity / 100}" :src="form.path" alt />
     </div>
-    <el-dialog title="设置" :visible.sync="isSlideOpen" class="dialog" width="90%" >
+    <el-form ref="form" :model="form" label-width="80px" class="upload-layer">
+      <el-upload class="upload" drag action :show-file-list="false" :http-request="this.handlePic">
+        <i class="el-icon-upload"></i>
+        <div class="el-upload__text">
+          将文件拖到此处，或
+          <em>点击上传</em>
+        </div>
+      </el-upload>
+    </el-form>
+    <!-- <el-dialog title="设置" :visible.sync="isSlideOpen" class="dialog" width="90%" >
     <div class="tool">
-      <el-form ref="form" :model="form" label-width="80px">
-        <el-upload
-          class="upload"
-          drag
-          action=""
-          :show-file-list="false"
-          :http-request="this.handlePic"
-        >
-          <i class="el-icon-upload"></i>
-          <div class="el-upload__text">
-            将文件拖到此处，或
-            <em>点击上传</em>
-          </div>
-        </el-upload>
-        <el-form-item label="透明度">
+
+    </div>
+    </el-dialog>-->
+    <!-- <el-form-item label="透明度">
           <div class="slider-box">
             <el-slider v-model="form.opacity" class="slide-tool"></el-slider>
             {{form.opacity}}%
           </div>
-        </el-form-item>
-      </el-form>
-    </div>
-    </el-dialog>
-
+    </el-form-item>-->
   </div>
 </template>
 
 <script>
+const { ipcRenderer } = require("electron");
 export default {
   name: "MainWrap",
   data() {
@@ -49,6 +44,17 @@ export default {
         desc: ""
       }
     };
+  },
+  created: () => {
+  },
+  mounted(){
+    ipcRenderer.on("keyda", (event, arg) => {
+      if(arg === 'Up') {
+        this.form.opacity += 10;
+      } else if('Down') {
+        this.form.opacity -= 10
+      }
+    });
   },
   methods: {
     open(link) {
@@ -71,21 +77,37 @@ export default {
   padding: 0;
 }
 
-body {
+::-webkit-scrollbar {
+    display: none;
+}
+body{
+  overflow: hidden;
 }
 
 #wrapper {
   height: 100vh;
   width: 100%;
   border: 1px solid rgb(170, 128, 128);
+  overflow: hidden;
 }
 
 div.dialog {
   margin-top: 0 !important;
 }
 
-.drawer {
-  /* background: #fff; */
+.upload-layer {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+}
+
+.upload,
+.el-upload,
+.upload .el-upload-dragger {
+  width: 100%;
+  height: 100%;
+  opacity: 0;
 }
 
 .cover {
@@ -96,15 +118,6 @@ div.dialog {
   padding: 20px;
 }
 
-.upload {
-  margin-bottom: 30px;
-  display: flex;
-  justify-content: center;
-}
-
-.upload .el-upload-dragger {
-  width: 200px;
-}
 .slider-box {
   display: flex;
 }
@@ -117,13 +130,6 @@ div.dialog {
 .coverPic {
   width: 100%;
   opacity: 0.5;
-  min-width: 500px;
-}
-
-#logo {
-  height: auto;
-  margin-bottom: 20px;
-  width: 420px;
 }
 
 main {
@@ -151,34 +157,5 @@ main > div {
   font-size: 20px;
   font-weight: bold;
   margin-bottom: 6px;
-}
-
-.title.alt {
-  font-size: 18px;
-  margin-bottom: 10px;
-}
-
-.doc p {
-  color: black;
-  margin-bottom: 10px;
-}
-
-.doc button {
-  font-size: 0.8em;
-  cursor: pointer;
-  outline: none;
-  padding: 0.75em 2em;
-  border-radius: 2em;
-  display: inline-block;
-  color: #fff;
-  background-color: #4fc08d;
-  transition: all 0.15s ease;
-  box-sizing: border-box;
-  border: 1px solid #4fc08d;
-}
-
-.doc button.alt {
-  color: #42b983;
-  background-color: transparent;
 }
 </style>
